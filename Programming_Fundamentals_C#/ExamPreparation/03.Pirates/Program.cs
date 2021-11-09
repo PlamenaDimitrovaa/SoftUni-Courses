923949d35e3f824155e14da3d7ee9d5181d6ea55
Plamena <plamena3456@abv.bg>
2021-11-09 16:40:17 +0200

    Added exam preparation for Programming Fundamentals course.

---
+++ Programming_Fundamentals_C#/ExamPreparation/03.Pirates/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Pirates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int[]> pirates = new Dictionary<string, int[]>();
+
+            while (input != "Sail")
+            {
+                string[] inputInfo = input.Split("||");
+                string city = inputInfo[0];
+                int population = int.Parse(inputInfo[1]);
+                int gold = int.Parse(inputInfo[2]);
+
+                if (!pirates.ContainsKey(city))
+                {
+                    pirates.Add(city, new int[] { population, gold });
+                }
+                else
+                {
+                    int[] currentValues = pirates[city];
+                    currentValues[0] += population;
+                    currentValues[1] += gold;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputInfo = input.Split("=>");
+                string command = inputInfo[0];
+
+                if (command == "Plunder")
+                {
+                    string city = inputInfo[1];
+                    int people = int.Parse(inputInfo[2]);
+                    int gold = int.Parse(inputInfo[3]);
+
+                    int[] values = pirates[city];
+                    values[0] -= people;
+                    values[1] -= gold;
+                    Console.WriteLine($"{city} plundered! {gold} gold stolen, {people} citizens killed.");
+
+                    if (values[0] == 0 || values[1] == 0)
+                    {
+                        Console.WriteLine($"{city} has been wiped off the map!");
+                        pirates.Remove(city);
+                    }
+                }
+                else if (command == "Prosper")
+                {
+                    string city = inputInfo[1];
+                    int gold = int.Parse(inputInfo[2]);
+
+                    if (gold < 0)
+                    {
+                        Console.WriteLine("Gold added cannot be a negative number!");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+
+                    int[] values = pirates[city];
+                    values[1] += gold;
+
+                    Console.WriteLine($"{gold} gold added to the city treasury. {city} now has {values[1]} gold.");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (pirates.Count == 0)
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
+            }
+            else
+            {
+                Console.WriteLine($"Ahoy, Captain! There are {pirates.Count} wealthy settlements to go to:");
+                foreach (var p in pirates.OrderByDescending(x => x.Value[1]).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{p.Key} -> Population: {p.Value[0]} citizens, Gold: {p.Value[1]} kg");
+                }
+
+            }
+        }
+    }
+}
---
+++ Programming_Fundamentals_C#/ExamPreparation/ExamPreparation/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+
+            while (command != "Generate")
+            {
+                var splitted = command.Split(">>>");
+                string action = splitted[0];
+
+
+                if (action == "Contains")
+                {
+                    string substring = splitted[1];
+
+                    if (input.Contains(substring))
+                    {
+                        Console.WriteLine($"{input} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (action == "Flip")
+                {
+                    string upperLower = splitted[1];
+                    int startIndex = int.Parse(splitted[2]);
+                    int endIndex = int.Parse(splitted[3]);
+                    if (upperLower == "Upper")
+                    {
+                        for (int i = startIndex; i < endIndex; i++)
+                        {
+                            char currentChar = char.ToUpper(input[i]);
+                            input = input.Remove(i, 1);
+                            input = input.Insert(i, currentChar.ToString());
+                           
+                        }
+                    }
+                    else if (upperLower == "Lower")
+                    {
+                        for (int i = startIndex; i < endIndex; i++)
+                        {
+                            char currentChar = char.ToLower(input[i]);
+                            input = input.Remove(i, 1);
+                            input = input.Insert(i, currentChar.ToString());
+
+                        }
+                    }
+
+                    Console.WriteLine(input);
+                }
+                else if (action == "Slice")
+                {
+                    int startIndex = int.Parse(splitted[1]);
+                    int endIndex = int.Parse(splitted[2]);
+                    for (int i = endIndex - 1; i >= startIndex; i--)
+                    {
+                        input = input.Remove(i, 1);
+                    }
+
+                    Console.WriteLine(input);
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your activation key is: {input}");
+        }
+    }
+}
