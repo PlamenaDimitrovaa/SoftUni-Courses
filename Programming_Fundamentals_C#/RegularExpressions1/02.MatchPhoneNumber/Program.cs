3b14954d13a6429d139d7bef351eed5240a56cb6
Plamena <plamena3456@abv.bg>
2021-11-09 15:27:06 +0200

    Added more exercises from Programming Fundamentals course(RegularExpressions)

---
+++ Programming_Fundamentals_C#/RegularExpressions1/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var regex = @"\+359(?<separator>(.?)(-?))2(\k<separator>(.?)(-?))(?<firstgroup>[0-9]{3})(\k<separator>(.?)(-?))(?<secondgroup>[0-9]{4})";
+
+            var phoneMatches = Regex.Matches(input, regex);
+
+            var matchedPhones = phoneMatches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(String.Join(", ", matchedPhones));
+        }
+    }
+}
---
+++ Programming_Fundamentals_C#/RegularExpressions1/RegularExpressions1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            MatchCollection matchedNames = Regex.Matches(input, regex);
+
+            foreach (Match match in matchedNames)
+            {
+                Console.Write(match.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
