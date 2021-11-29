baf982488e074865b7c8153b6c872b623afca6d2
Plamena <plamena3456@abv.bg>
2021-11-29 23:00:33 +0200

    Added streams,files and directories exercises.

---
+++ C#_Advanced/StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new StreamReader("countries.txt");
+            using StreamWriter sw = new StreamWriter("countriesWithLineNumber.txt");
+            int rowNum = 1;
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                sw.WriteLine($"{rowNum}. {line}");
+                rowNum++;
+            }
+
+        }
+    }
+}
---
+++ C#_Advanced/StreamsFilesAndDirectories/StreamsFilesAndDirectories/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace StreamsFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new StreamReader("countries.txt");
+            using StreamWriter sw = new StreamWriter("oddCountries.txt");
+            int rowNum = 0;
+        
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                if (rowNum % 2 == 1)
+                {
+                    sw.WriteLine(line);
+                }
+
+                rowNum++;
+            }
+
+        }
+    }
+}
