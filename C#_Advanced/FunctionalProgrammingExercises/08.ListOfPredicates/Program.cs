d10ce882d1712d10e3287611eb5e7f396cafea08
Plamena <plamena3456@abv.bg>
2021-12-05 23:00:02 +0200

    Added functional programming exercises.

---
+++ C#_Advanced/FunctionalProgrammingExercises/08.ListOfPredicates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] divNums = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+            int[] allNumbers = Enumerable.Range(1, n).ToArray();
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            foreach (var number in divNums)
+            {
+                predicates.Add(x => x % number == 0);
+            }
+
+            foreach (var currentNumber in allNumbers)
+            {
+                bool isDivisible = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(currentNumber))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write(currentNumber + " ");
+                }
+            }
+        }
+    }
+}
---
+++ C#_Advanced/FunctionalProgrammingExercises/11.TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _11.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+            Func<string, int, bool> isLargerOrEqualToNameASCII = (name, asciiSum)
+                => name.Sum(x => x) >= asciiSum;
+
+            Func<List<string>, int, Func<string, int, bool>, string> desiredName = (names, number, func)
+                => names.FirstOrDefault(x => func(x,number));
+
+            string name = desiredName(names, n, isLargerOrEqualToNameASCII);
+            Console.WriteLine(name);
+
+        }
+    }
+}
