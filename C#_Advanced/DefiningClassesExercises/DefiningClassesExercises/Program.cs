37995f6bc943c21e20aadeb5e8d6c217f71ba0df
Plamena <plamena3456@abv.bg>
2021-12-10 22:57:54 +0200

    Added defining classes exercises.

---
+++ C#_Advanced/DefiningClassesExercises/DefiningClassesExercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        public class Person
+        {
+            private string name;
+            private int age;
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    age = value;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var person = new Person("Peter", 20);
+            var person2 = new Person("George", 18);
+            var person3 = new Person("Jose", 43);
+        }
+    }
+}
