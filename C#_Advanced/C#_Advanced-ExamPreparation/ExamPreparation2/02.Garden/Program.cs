ba63d8d0bfb8d433ec2741ff9979466c02d55c3b
Plamena <plamena3456@abv.bg>
2021-12-16 22:42:39 +0200

    Added C#_Advanced exam preparation.

---
+++ C#_Advanced/C#_Advanced-ExamPreparation/ExamPreparation2/02.Garden/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimension = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int isRow = dimension[0];
+            int isCol = dimension[1];
+            int[,] garden = new int[isRow, isCol];
+            for (int i = 0; i < isRow; i++)
+            {
+                for (int j = 0; j < isCol; j++)
+                {
+                    garden[i, j] = 0;                  
+                }
+            }
+
+           
+            while (true)
+            {
+                var position = Console.ReadLine();
+                if (position == "Bloom Bloom Plow")
+                {
+                    break;
+                }            
+                var positionInfo = position.Split();
+                var row = int.Parse(positionInfo[0]);
+                var col = int.Parse(positionInfo[1]);
+
+                if (row < 0 || row > garden.GetLength(0) && col < 0 || col > garden.GetLength(1))
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+
+                for (int i = 0; i < garden.GetLength(0); i++)
+                {
+                    garden[row, i]++;
+                }
+                for (int j   = 0; j < garden.GetLength(1); j++)
+                {
+                    garden[j, col]++;
+                }
+                garden[row, col]--;
+            }
+
+            for (int i = 0; i < isRow; i++)
+            {
+                for (int j = 0; j < isCol; j++)
+                {
+                    Console.Write(garden[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#_Advanced/C#_Advanced-ExamPreparation/ExamPreparation2/ExamPreparation2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPreparation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var threads = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int taskToKill = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>(tasks);
+            Queue<int> queue = new Queue<int>(threads);
+            var thread = 0;
+            var task = 0;
+            while (stack.Contains(taskToKill))
+            {
+                 thread = queue.Peek();
+                 task = stack.Peek();
+
+                if (task == taskToKill)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
+                    break;
+                }
+                if (thread >= task)
+                {
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else if (thread < task)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", queue));
+
+        }
+    }
+}
