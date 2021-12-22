e61924bb2d1486de1f2fbd58564e81a9aee20cf9
Plamena <plamena3456@abv.bg>
2021-12-22 18:35:08 +0200

    Added C#_Advanced exam preparation.

---
+++ C#_Advanced/C#_Advanced-ExamPreparation/ExamPreparation5/02.Selling/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            int row = 0;
+            int col = 0;
+            int food = 0;
+
+            for (int rows = 0; rows < n; rows++)
+            {
+                string input = Console.ReadLine();
+                for (int cols = 0; cols < n; cols++)
+                {
+                    matrix[rows, cols] = input[cols];
+
+                    if (matrix[rows, cols] == 'S')
+                    {
+                        row = rows;
+                        col = cols;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "up")
+                {
+                    matrix[row, col] = '-';
+                    row = row - 1; 
+                    if (IsPositionValid(row, col, n, n)) 
+                    {
+                        SnakeMoves(n, matrix, ref row, ref col, ref food);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {food}");
+                        break;
+                    }
+                }
+                else if (command == "down")
+                {
+                    matrix[row, col] = '-'; 
+                    row = row + 1; 
+                    if (IsPositionValid(row, col, n, n)) 
+                    {
+                        SnakeMoves(n, matrix, ref row, ref col, ref food);
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {food}");
+                        break;
+                    }
+                }
+                else if (command == "left")
+                {
+                    matrix[row, col] = '-';
+                    col = col - 1; 
+                    if (IsPositionValid(row, col, n, n)) 
+                    {
+                        SnakeMoves(n, matrix, ref row, ref col, ref food);
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {food}");
+                        break;
+                    }
+                }
+                else if (command == "right")
+                {
+                    matrix[row, col] = '-';
+                    col = col + 1; 
+                    if (IsPositionValid(row, col, n, n))
+                    {
+                        SnakeMoves(n, matrix, ref row, ref col, ref food);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {food}");
+                        break;
+                    }
+                }
+
+                if (food >= 50)
+                {
+                    Console.WriteLine("Good news! You succeeded in collecting enough money!");
+                    Console.WriteLine($"Money: {food}");
+                    matrix[row, col] = 'S';
+                    break;
+                }
+
+            }
+
+            PrintMatrix(n, matrix);
+        }
+
+        private static void SnakeMoves(int n, char[,] matrix, ref int row, ref int col, ref int food)
+        {
+            if (char.IsDigit(matrix[row, col]))
+            {
+                food += int.Parse(matrix[row, col].ToString());
+            }
+            else if (matrix[row, col] == 'O') 
+            {
+                matrix[row, col] = '-';
+                for (int rows = 0; rows < n; rows++)
+                {
+                    for (int cols = 0; cols < n; cols++)
+                    {
+                        if (matrix[rows, cols] == 'O')
+                        {
+                            matrix[rows, cols] = '-';
+                            row = rows;
+                            col = cols;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void PrintMatrix(int n, char[,] matrix)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row >= 0 && row < rows && col >= 0 && col < cols)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+    
+        }
+    }
+}
---
+++ C#_Advanced/C#_Advanced-ExamPreparation/ExamPreparation5/ExamPreparation5/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPreparation5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var liquids = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var ingredients = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(liquids);
+            Stack<int> stack = new Stack<int>(ingredients);
+
+            int breadCount = 0;
+            int cakeCount = 0;
+            int pastryCount = 0;
+            int fruitpieCount = 0;
+
+            while (queue.Any() && stack.Any())
+            {
+                var currentSum = queue.Peek() + stack.Peek();
+                if (currentSum == 25)
+                {
+                    breadCount++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else if (currentSum == 50)
+                {
+                    cakeCount++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else if (currentSum == 75)
+                {
+                    pastryCount++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else if (currentSum == 100)
+                {
+                    fruitpieCount++;
+                    queue.Dequeue();
+                    stack.Pop();
+                }
+                else
+                {
+                    queue.Dequeue();
+                    var removed = stack.Pop();
+                    stack.Push(removed + 3);
+                }
+            }
+
+            if (breadCount >= 1 && cakeCount >= 1 && pastryCount >= 1 && fruitpieCount >= 1)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (queue.Any())
+            {
+                Console.WriteLine($"Liquids left: {String.Join(", ", queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (stack.Any())
+            {
+                Console.WriteLine($"Ingredients left: {String.Join(", ", stack)}");
+            }
+            else
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+
+            Console.WriteLine($"Bread: {breadCount}");
+            Console.WriteLine($"Cake: {cakeCount}");
+            Console.WriteLine($"Fruit Pie: {fruitpieCount}");
+            Console.WriteLine($"Pastry: {pastryCount}");
+
+        }
+    }
+}
