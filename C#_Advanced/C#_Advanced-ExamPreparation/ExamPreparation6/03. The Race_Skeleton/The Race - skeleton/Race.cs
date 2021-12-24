3060d2337e10042041defc927c98cea601abe5b6
Plamena <plamena3456@abv.bg>
2021-12-24 14:57:58 +0200

    Added C#_Advanced exam preparation.

---
+++ C#_Advanced/C#_Advanced-ExamPreparation/ExamPreparation6/03. The Race_Skeleton/The Race - skeleton/Race.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> data;
+
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public string Name  { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count();
+        public void Add(Racer racer)
+        {
+            if (Capacity > data.Count)
+            {
+                data.Add(racer);
+            }
+        }
+        public bool Remove(string name)
+        {        
+            if (data.Exists(x => x.Name == name))
+            {
+                data.Remove(data.FirstOrDefault(x => x.Name == name));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Racer GetOldestRacer()
+        {
+            var oldest = data.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldest;
+        }
+
+        public Racer GetRacer(string name)
+        {
+            var racer = data.FirstOrDefault(x => x.Name == name);
+            return racer;
+        }
+
+        public Racer GetFastestRacer()
+        {
+            var racer = data.OrderByDescending(x => x.Car.Speed).FirstOrDefault();
+            return racer;
+          
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Racers participating at {Name}:");
+            foreach (Racer racer in data)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
