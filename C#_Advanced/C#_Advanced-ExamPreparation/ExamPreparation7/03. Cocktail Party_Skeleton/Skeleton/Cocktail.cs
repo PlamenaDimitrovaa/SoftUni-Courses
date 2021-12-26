b2ccb8c953cbf1be3a2931b3d7bf71134a9b9be8
Plamena <plamena3456@abv.bg>
2021-12-26 16:48:51 +0200

    Added C#_Advanced exam preparation.

---
+++ C#_Advanced/C#_Advanced-ExamPreparation/ExamPreparation7/03. Cocktail Party_Skeleton/Skeleton/Cocktail.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        private List<Ingredient> Ingredients;
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            Ingredients = new List<Ingredient>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+        public int CurrentAlcoholLevel => Ingredients.Sum(x => x.Alcohol);
+
+        public void Add(Ingredient ingredient)
+        {
+            if (Capacity > Ingredients.Count && (!Ingredients.Exists(x => x.Name == ingredient.Name)) && MaxAlcoholLevel > CurrentAlcoholLevel)
+            {
+                Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (Ingredients.Exists(x => x.Name == name))
+            {
+                Ingredients.Remove(Ingredients.FirstOrDefault(x => x.Name == name));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            var ingredient = Ingredients.FirstOrDefault(x => x.Name == name);
+            return ingredient;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            var ingredient = Ingredients.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+            return ingredient;
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+            foreach (var item in Ingredients)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
