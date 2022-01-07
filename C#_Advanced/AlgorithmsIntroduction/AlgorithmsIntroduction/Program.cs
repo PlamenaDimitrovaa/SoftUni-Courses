d8dc324bfed228dedc7d4c241226a2f7a629547f
Plamena <plamena3456@abv.bg>
2022-01-07 22:21:56 +0200

    Added Algorithms Introduction C#_Advanced.

---
+++ C#_Advanced/AlgorithmsIntroduction/AlgorithmsIntroduction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmsIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(inputArray, 0));  
+        }
+
+        private static int Sum(int[] array, int currentIndex)
+        {
+            if (currentIndex == array.Length)
+            {
+                return 0;
+            }
+
+            return array[currentIndex] + Sum(array, currentIndex + 1);
+
+        }
+    }
+}
