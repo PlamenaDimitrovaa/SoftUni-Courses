a11e7acbf275bdccea2e75665371fd985dd9a74f
Plamena <plamena3456@abv.bg>
2022-01-08 23:09:37 +0200

    Added Algorithms Introduction C#_Advanced.

---
+++ C#_Advanced/AlgorithmsIntroduction/03.SumOfCoins/03. Sum of Coins_Skeleton/SumOfCoins/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+namespace SumOfCoins
+{
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var coins = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int targetSum = int.Parse(Console.ReadLine());
+            Dictionary<int, int> result = ChooseCoins(coins, targetSum);
+            Console.WriteLine($"Number of coins to take: {result.Sum(x => x.Value)}");
+            foreach (var item in result.OrderByDescending(x => x.Key))
+            {
+                Console.WriteLine($"{item.Value} coin(s) with value {item.Key}");
+            }
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+
+            coins = coins.OrderBy(x => x).ToList();
+            int index = coins.Count - 1;
+            Dictionary<int, int> coinsCount = new Dictionary<int, int>();
+            while (index > -1)
+            {
+                int currentCoin = coins[index];
+                int result = targetSum / currentCoin;
+
+                if (result < 1)
+                {
+                    index--;
+                    continue;
+                }
+
+                coinsCount.Add(currentCoin, result);
+                targetSum -= currentCoin * result;
+
+                if (targetSum == 0)
+                {
+                    break;
+                }
+            }
+
+            if (targetSum > 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return coinsCount;
+        }
+    }
+}
---
+++ C#_Advanced/AlgorithmsIntroduction/04.SetCoverSkeleton/SetCover/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+namespace SetCover
+{
+    using System.Collections.Generic;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var universe = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+            List<int[]> sets = new List<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var set = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sets.Add(set);
+            }
+  
+            var result = ChooseSets(sets, universe);
+            Console.WriteLine($"Sets to take ({result.Count}):");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", item)} }}");
+            }
+        }
+
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+            List<int[]> result = new List<int[]>();
+
+            while (sets.Count > 0 && universe.Count > 0)
+            {
+                int[] largestSubsetOfUniverse = sets.OrderByDescending
+                    (set => set.Count(el => universe.Contains(el))).FirstOrDefault();
+
+                foreach (var item in largestSubsetOfUniverse)
+                {
+                    universe.Remove(item);
+                }
+
+                result.Add(largestSubsetOfUniverse);
+                sets.Remove(largestSubsetOfUniverse);
+            }
+
+            return result;
+        }
+    }
+}
