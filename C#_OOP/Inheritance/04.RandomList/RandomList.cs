4fca42953c01a2a1d1edc06445a57db6538194ce
Plamena <plamena3456@abv.bg>
2022-01-11 20:30:57 +0200

    Added C#_OOP Inheritance Lab exercises.

---
+++ C#_OOP/Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public Random random { get; set; }
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+        public string RandomString()
+        {
+            var index = random.Next(0, this.Count);
+            return this[index];
+            this.RemoveAt(index);
+        }
+    }
+}
