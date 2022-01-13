f98a7bf56bea278ae76f008d96b2ebc521543f61
Plamena <plamena3456@abv.bg>
2022-01-13 20:21:02 +0200

    Added C#_OOP Encapsulation Lab Exercises.

---
+++ C#_OOP/Encapsulation/04.Team/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var team = new Team("SoftUni");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split();
+                var person = new Person(line[0], line[1], int.Parse(line[2]), decimal.Parse(line[3]));
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
