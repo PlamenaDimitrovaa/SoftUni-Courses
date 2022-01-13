a103ef428798ae560003e61b517721b695552e11
Plamena <plamena3456@abv.bg>
2022-01-13 20:28:43 +0200

    Added Encapsulation exercises.

--- C#_OOP/Encapsulation/Encapsulation/Person.cs
+++ C#_OOP/Encapsulation/Encapsulation/Person.cs
@@ -12,7 +12,6 @@ namespace PersonsInfo
             LastName = lastName;
             Age = age;
         }
-
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public int Age { get; private set; }
