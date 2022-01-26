ddb2672eb8de7429914af852c3585640bdc00d2b
Plamena <plamena3456@abv.bg>
2022-01-26 22:59:25 +0200

    Added Exception Handling exercises.

---
+++ C#_OOP/ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                int squareRoot = (int)Math.Sqrt(num);
+                Console.WriteLine(squareRoot);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
