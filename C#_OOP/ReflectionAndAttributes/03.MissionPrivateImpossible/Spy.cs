43837734f0099c5386dafb55758db606de7b0a55
Plamena <plamena3456@abv.bg>
2022-02-01 23:10:59 +0200

    Added Reflection and Attributes Exercises.

---
+++ C#_OOP/ReflectionAndAttributes/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"All Private Methods of Class: {investigatedClass}")
+                .AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (MethodInfo method in classMethods)
+            {
+                builder.AppendLine(method.Name);
+            }
+
+            return builder.ToString().Trim();
+        }
+    }
+}
