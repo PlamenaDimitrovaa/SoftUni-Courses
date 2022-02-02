d875bd97a39c349c27a330e98cd1f09af4566259
Plamena <plamena3456@abv.bg>
2022-02-02 12:27:09 +0200

    Added Reflection and Attributes exercises.

---
+++ C#_OOP/ReflectionAndAttributes/05.CreateAttribute/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    using System;
+
+    [Author("Victor")]
+    class Program
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
