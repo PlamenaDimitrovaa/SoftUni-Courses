8b37c52c416a39cff8b097d8f1d426a21ab5a034
Plamena <plamena3456@abv.bg>
2022-02-06 22:14:15 +0200

    Added Unit Testing Exercises.

---
+++ C#_OOP/UnitTesting/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
