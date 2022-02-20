00e5804283a47d64708f9f87c192dd5cb43be931
Plamena <plamena3456@abv.bg>
2022-02-20 13:18:23 +0200

    Added exam problems(20.02.2022).

---
+++ C#_Advanced/C#_Advanced-ExamPreparation/Exam(20.02.2022)/01.FirstProblem/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.FirstProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var water = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            var flour = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            var queueWater = new Queue<double>(water);
+            var stackFlour = new Stack<double>(flour);
+            var croissant = 0;
+            var muffin = 0;
+            var baguette = 0;
+            var bagel = 0;
+
+            while (queueWater.Any() && stackFlour.Any())
+            {
+                var mix = queueWater.Peek() + stackFlour.Peek();
+
+                if (((queueWater.Peek() * 100) / mix) == 50 && ((stackFlour.Peek() * 100) / mix) == 50)
+                {
+                    croissant++;
+                    queueWater.Dequeue();
+                    stackFlour.Pop();
+                }
+                else if (((queueWater.Peek() * 100) / mix) == 40 && ((stackFlour.Peek() * 100) / mix) == 60)
+                {
+                    muffin++;
+                    queueWater.Dequeue();
+                    stackFlour.Pop();
+                }
+                else if (((queueWater.Peek() * 100) / mix) == 30 && ((stackFlour.Peek() * 100) / mix) == 70)
+                {
+                    baguette++;
+                    queueWater.Dequeue();
+                    stackFlour.Pop();
+                }
+                else if (((queueWater.Peek() * 100) / mix) == 20 && ((stackFlour.Peek() * 100) / mix) == 80)
+                {
+                    bagel++;
+                    queueWater.Dequeue();
+                    stackFlour.Pop();
+                }
+                else
+                {
+                    if (stackFlour.Peek() > queueWater.Peek())
+                    {
+                        croissant++;
+                        var toInsert = stackFlour.Pop() - queueWater.Dequeue();
+                        stackFlour.Push(toInsert);
+                    }
+                }
+            }
+
+            var dictionary = new Dictionary<string, double>();
+            dictionary.Add("Croissant", croissant);
+            dictionary.Add("Muffin", muffin);
+            dictionary.Add("Baguette", baguette);
+            dictionary.Add("Bagel", bagel);
+
+            foreach (var item in dictionary.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                if (item.Value > 0)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+
+            if (queueWater.Any())
+            {
+                Console.WriteLine($"Water left: {string.Join(", ", queueWater)}");
+            }
+            else
+            {
+                Console.WriteLine("Water left: None");
+            }
+
+            if (stackFlour.Any())
+            {
+                Console.WriteLine($"Flour left: {string.Join(", ", stackFlour)}");
+            }
+            else
+            {
+                Console.WriteLine("Flour left: None");
+            }
+        }
+    }
+}
---
+++ C#_Advanced/C#_Advanced-ExamPreparation/Exam(20.02.2022)/02.SecondProblem/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SecondProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] pond = new char[n, n];
+            var branches = new Stack<char>();
+            var totalBranches = 0;
+            var beaverRow = -1;
+            var beaverCol = -1;
+            for (int row = 0; row < n; row++)
+            {
+                char[] line = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for (int col = 0; col < line.Length; col++)
+                {
+                    pond[row, col] = line[col];
+
+                    if (pond[row, col] == 'B')
+                    {
+                        beaverRow = row;
+                        beaverCol = col;
+                    }
+
+                    if (char.IsLower(pond[row, col]))
+                    {
+                        totalBranches++;
+                    }
+                }
+            }
+
+            var command = Console.ReadLine();
+            while (command != "end")
+            {
+                pond[beaverRow, beaverCol] = '-';
+                if (command == "up")
+                {
+                    if (IsPositionValid(pond, beaverRow - 1, beaverCol))
+                    {
+                        beaverRow = MoveRow(beaverRow, command);
+                    }
+                    else
+                    {
+                        if (branches.Any())
+                        {
+                            branches.Pop();
+                        }
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (IsPositionValid(pond, beaverRow + 1, beaverCol))
+                    {
+                        beaverRow = MoveRow(beaverRow, command);
+                    }
+                    else
+                    {
+                        if (branches.Any())
+                        {
+                            branches.Pop();
+                        }
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (IsPositionValid(pond, beaverRow, beaverCol - 1))
+                    {
+                        beaverCol = MoveCol(beaverCol, command);
+                    }
+                    else
+                    {
+                        if (branches.Any())
+                        {
+                            branches.Pop();
+                        }
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (IsPositionValid(pond, beaverRow, beaverCol + 1))
+                    {
+                        beaverCol = MoveCol(beaverCol, command);
+
+                    }
+                    else
+                    {
+                        if (branches.Any())
+                        {
+                            branches.Pop();
+                        }
+                    }
+                }
+
+                if (char.IsLower(pond[beaverRow, beaverCol]))
+                {
+                    branches.Push(pond[beaverRow, beaverCol]);
+                }
+                if (pond[beaverRow, beaverCol] == 'F')
+                {
+                    if (command == "up" && beaverRow == 0)
+                    {
+                        command = "down";
+                    }
+                    else if (command == "down" && beaverRow == pond.GetLength(0))
+                    {
+                        command = "up";
+                    }
+                    else if (command == "left" && beaverCol == 0)
+                    {
+                        command = "right";
+                    }
+                    else if (command == "right" && beaverCol == pond.GetLength(1))
+                    {
+                        command = "left";
+                    }
+                    while (true)
+                    {
+                        var lastRow = beaverRow;
+                        var lastCol = beaverCol;
+                        if (command == "up" || command == "down")
+                        {
+                            beaverRow = MoveRow(beaverRow, command);
+                        }
+                        else if (command == "left" || command == "right")
+                        {
+                            beaverCol = MoveCol(beaverCol, command);
+                        }
+                        if (char.IsLower(pond[beaverRow, beaverCol]))
+                        {
+                            branches.Push(pond[beaverRow, beaverCol]);
+                        }
+                        if (lastRow == beaverRow && lastCol == beaverCol)
+                        {
+                            break;
+                        }
+
+                        pond[beaverRow, beaverCol] = '-';
+                    }
+                }
+
+                pond[beaverRow, beaverCol] = 'B';
+                command = Console.ReadLine();
+            }
+
+            if (totalBranches == branches.Count())
+            {
+                Console.WriteLine($"The Beaver successfully collect {branches.Count()} wood branches: {string.Join(", ", branches)}.");
+            }
+            else
+            {
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {totalBranches - branches.Count()} branches left.");
+            }
+            Print(pond);
+
+            static void Print(char[,] matrix)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write(matrix[row, col] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            static bool IsPositionValid(char[,] matrix, int row, int col)
+            {
+                if (row < 0 || col < 0)
+                {
+                    return false;
+                }
+                if (row >= matrix.GetLength(0) || col >= matrix.GetLength(1))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            static int MoveRow(int row, string movement)
+            {
+                if (movement == "up")
+                {
+                    return row - 1;
+                }
+                if (movement == "down")
+                {
+                    return row + 1;
+                }
+                return row;
+            }
+
+            static int MoveCol(int col, string movement)
+            {
+                if (movement == "left")
+                {
+                    return col - 1;
+                }
+                if (movement == "right")
+                {
+                    return col + 1;
+                }
+
+                return col;
+            }
+        }
+    }
+}
