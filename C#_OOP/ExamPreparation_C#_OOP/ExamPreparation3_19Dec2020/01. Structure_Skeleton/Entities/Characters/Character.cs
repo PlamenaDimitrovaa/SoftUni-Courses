63332bd849aae81867b23fd7b92084005c033843
Plamena <plamena3456@abv.bg>
2022-03-13 19:51:30 +0200

    Added C#_OOP exam preparation.

---
+++ C#_OOP/ExamPreparation_C#_OOP/ExamPreparation3_19Dec2020/01. Structure_Skeleton/Entities/Characters/Character.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+using WarCroft.Constants;
+using WarCroft.Entities.Inventory;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+		// TODO: Implement the rest of the class.
+		private readonly double baseHealth;
+		private readonly double baseArmor;
+		protected readonly double abilityPoints;
+
+        public Character(string name, double health, double armor, double abilityPoints, Bag bag)
+        {
+			Name = name;
+			Health = health;
+			baseArmor = armor;
+			Armor = armor;
+			this.abilityPoints = abilityPoints;
+			this.Bag = bag;
+        }
+
+		private string name;
+		private double health;
+		private double armor;
+        public string Name
+		{
+            get
+            {
+				return name;
+            }
+			private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+					throw new ArgumentException(ExceptionMessages.CharacterNameInvalid);
+                }
+
+				name = value;
+            } 
+		}
+        public double Health
+		{
+			get => health;
+            set
+            {
+                if (value < 0)
+                {
+					health = 0;
+                }
+                else if (value > baseHealth)
+                {
+					health = baseHealth;
+                }
+                else
+                {
+					health = value;
+                }
+            }
+		}
+		public double Armor
+		{
+			get => armor;
+			set
+			{
+				if (value < 0)
+				{
+					armor = 0;
+				}
+                else
+                {
+					armor = value;
+                }
+			}
+		}
+
+        public IBag Bag { get; }
+        public bool IsAlive { get; set; } = true;
+
+		public virtual void UseItem(Item item)
+        {
+			this.EnsureAlive();
+			item.AffectCharacter(this);
+        }
+
+		public virtual void TakeDamage(double hitpoints)
+        {
+			this.EnsureAlive();
+			double healthReduce = hitpoints - this.Armor;
+			this.Armor -= hitpoints;
+            if (healthReduce > 0)
+            {
+				this.Health -= healthReduce;
+            }
+
+            if (this.Health == 0)
+            {
+				this.IsAlive = false;
+            }
+        }
+
+		protected void EnsureAlive()
+		{
+			if (!this.IsAlive)
+			{
+				throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+			}
+		}
+	}
+}
---
+++ C#_OOP/ExamPreparation_C#_OOP/ExamPreparation3_19Dec2020/01. Structure_Skeleton/Entities/Inventory/BackPack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarCroft.Entities.Inventory
+{
+    public class BackPack : Bag
+    {
+        public BackPack()
+            : base(100)
+        {
+        }
+    }
+}
