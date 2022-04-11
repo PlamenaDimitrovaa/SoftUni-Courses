722024e6bd084558c9e5895a6f69ba9bf5e96e9f
Plamena <plamena3456@abv.bg>
2022-04-11 22:14:05 +0300

    Added BasicSyntax, Conditional statements and Loops - Lab.

---
+++ Programming_Fundamentals_with_JavaScript/BasicSyntax,ConditionalStatementsandLoops.js/06.TheatrePromotions.js
@@ -0,0 +1,50 @@
+function solve(type, age){
+    let price = 0;
+    if(type === 'Weekday'){
+        if(age >= 0 && age <= 18){
+            price = 12;
+        }
+        else if(age > 18 && age <= 64){
+            price = 18;
+        }
+        else if(age > 64 && age <= 122){
+            price = 12;
+        }
+        else{
+            console.log('Error!');
+            return;
+        }
+    }
+    else if(type === 'Weekend'){
+        if(age >= 0 && age <= 18){
+            price = 15;
+        }
+        else if(age > 18 && age <= 64){
+            price = 20;
+        }
+        else if(age > 64 && age <= 122){
+            price = 15;
+        }
+        else{
+            console.log('Error!');
+            return;
+        }
+    }
+    else if(type === 'Holiday'){
+        if(age >= 0 && age <= 18){
+            price = 5;
+        }
+        else if(age > 18 && age <= 64){
+            price = 12;
+        }
+        else if(age > 64 && age <= 122){
+            price = 10;
+        }
+        else{
+            console.log('Error!');
+            return;
+        }
+    }
+    
+    console.log(`${price}$`);
+}
---
+++ Programming_Fundamentals_with_JavaScript/BasicSyntax,ConditionalStatementsandLoops.js/09.NumbersfromNto1.js
@@ -0,0 +1,8 @@
+function solve(N){
+    while(N > 0){
+        console.log(N);
+        N--;
+    }
+}
+
+solve(5);
