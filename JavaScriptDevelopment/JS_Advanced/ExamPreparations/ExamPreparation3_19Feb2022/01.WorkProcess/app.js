470d4a7ad31566c1f7e617f92847c3c88fedb1a7
Plamena <plamena3456@abv.bg>
2022-06-11 17:16:53 +0300

    Modified solution.

--- JavaScriptDevelopment/JS_Advanced/ExamPreparations/ExamPreparation3_19Feb2022/01.WorkProcess/app.js
+++ JavaScriptDevelopment/JS_Advanced/ExamPreparations/ExamPreparation3_19Feb2022/01.WorkProcess/app.js
@@ -1,55 +1,86 @@
-// function solve() {
-//     let firstNameElement = document.getElementById('fname');
-//     let lastNameElement = document.getElementById('lname');
-//     let emailElement = document.getElementById('email');
-//     let dateOfBirthElement = document.getElementById('birth');
-//     let positionElement = document.getElementById('position');
-//     let salaryElement = document.getElementById('salary');
-//     let hireWorkerButton = document.getElementById('add-worker');
-//     let bodyElement = document.getElementById('tbody');
-//     let divElement = document.querySelector('.tbl-content');
+function solve() {
+    let fNameElement = document.getElementById('fname');
+    let lNameElement = document.getElementById('lname');
+    let emailElement = document.getElementById('email');
+    let birthElement = document.getElementById('birth');
+    let positionElement = document.getElementById('position');
+    let salaryElement = document.getElementById('salary');
+    let addWorkerButton = document.getElementById('add-worker');
+
+    const inputValues = Array.from(document.querySelector('#signup > form').querySelectorAll('input'));
     
-//     // if(firstNameElement.value == "" || lastNameElement.value == "" || emailElement.value == "" || dateOfBirthElement.value == "" || 
-//     // positionElement.value == "" || salaryElement.value == ""){
-//     //     return;        
-//     // }
+    addWorkerButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(fNameElement.value && lNameElement.value && emailElement.value && birthElement.value && positionElement.value && salaryElement.value){
+
+            let tableBody = document.getElementById('tbody');
     
-//     hireWorkerButton.addEventListener('click', (e) => {
-        
-//         e.preventDefault();
-        
-//         let trElement = document.createElement('tr');
-//         let tdFNameEl = document.createElement('td');
-//         let tdLNameEl = document.createElement('td');
-//         let emailEl = document.createElement('td');
-//         let birthEl = document.createElement('td');
-//         let positionEl = document.createElement('td');
-//         let salaryEl = document.createElement('td');
-//         let firedButton = document.createAttribute('button');
-//         let editButton = document.createAttribute('button');
+            let tr = document.createElement('tr');
+    
+            let fNameTd = document.createElement('td');
+            fNameTd.textContent = fNameElement.value;
+            let lNameTd = document.createElement('td');
+            lNameTd.textContent = lNameElement.value;
+            let emailTd = document.createElement('td');
+            emailTd.textContent = emailElement.value;
+            let birthTd = document.createElement('td');
+            birthTd.textContent = birthElement.value;
+            let positionTd = document.createElement('td');
+            positionTd.textContent = positionElement.value;
+            let salaryTd = document.createElement('td');
+            salaryTd.textContent = salaryElement.value;
+    
+            let buttonTd = document.createElement('td');
+    
+            let firedButton = document.createElement('button');
+            firedButton.className = 'fired';
+            firedButton.textContent = "Fired";
+    
+            let editButton = document.createElement('button');
+            editButton.className = 'edit';
+            editButton.textContent = "Edit";
+    
+            let sum = document.getElementById('sum');
 
-//         tdFNameEl.textContent = firstNameElement.value;
-//         tdLNameEl.textContent = lastNameElement.value;
-//         emailEl.textContent = emailElement.value;
-//         birthEl.textContent = dateOfBirthElement.value;
-//         positionEl.textContent = positionElement.value;
-//         salaryEl.textContent = salaryElement.value;
-//         // firedButton.setAttribute("class", "fired");
-//         // editButton.setAttribute("class", "edit");
-//         firedButton.textContent = 'Fired';
-//         editButton.textContent = 'Edit';
+            firedButton.addEventListener('click', (e) => {
+                e.target.parentElement.parentElement.remove();
+                let tdCollection = Array.from(e.target.parentElement.parentElement.querySelectorAll('td'));
+                sum.textContent = (Number(sum.textContent) - Number(tdCollection[5].textContent)).toFixed(2);
+            })
 
-//         trElement.appendChild(tdFNameEl);
-//         trElement.appendChild(tdLNameEl);
-//         trElement.appendChild(emailEl);
-//         trElement.appendChild(birthEl);
-//         trElement.appendChild(positionEl);
-//         trElement.appendChild(salaryEl);
-//         trElement.appendChild(firedButton);
-//         trElement.appendChild(editButton);
+            editButton.addEventListener('click', () => {
+                const tdItems = Array.from(tr.childNodes);
+                
+                for (let i = 0; i < tdItems.length - 1; i++) {
+                    inputValues[i].value = tdItems[i].textContent;
+                }
+                
+                tr.remove();
+                
+                sum.textContent = (Number(sum.textContent) - Number(inputValues[5].value)).toFixed(2);
+            })
+            
+            buttonTd.appendChild(firedButton);
+            buttonTd.appendChild(editButton);
+    
+            tr.appendChild(fNameTd);
+            tr.appendChild(lNameTd);
+            tr.appendChild(emailTd);
+            tr.appendChild(birthTd);
+            tr.appendChild(positionTd);
+            tr.appendChild(salaryTd);
+            tr.appendChild(buttonTd);
+    
+            tableBody.appendChild(tr);
+            sum.textContent = (Number(sum.textContent) + Number(salaryElement.value)).toFixed(2);
 
-//         bodyElement.appendChild(trElement);
-//         divElement.appendChild(bodyElement);
-//     })
-// }
-// solve()
+            fNameElement.value = '';
+            lNameElement.value = '';
+            emailElement.value = '';
+            birthElement.value = '';
+            positionElement.value = '';
+            salaryElement.value = '';
+        }
+    })
+}
+solve()
