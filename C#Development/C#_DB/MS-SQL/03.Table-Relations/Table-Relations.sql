6ce713ff9acc1fdf032cadcb4310724c01be8504
Plamena Dimitrova <92258965+PlamenaDimitrovaa@users.noreply.github.com>
2022-08-25 20:04:31 +0300

    Added Table Relations Exercises.

--- C#Development/C#_DB/MS-SQL/03.Table-Relations/Table-Relations.sql
+++ C#Development/C#_DB/MS-SQL/03.Table-Relations/Table-Relations.sql
@@ -66,9 +66,83 @@
 
 
 --05. Online Store Database:
+--CREATE TABLE Cities
+--(
+--    CityID INT PRIMARY KEY IDENTITY,
+--	Name VARCHAR(50) NOT NULL
+--)
 
+--CREATE TABLE Customers
+--(
+--    CustomerID INT PRIMARY KEY IDENTITY,
+--	Name VARCHAR(40) NOT NULL,
+--	Birthday DATE,
+--	CityID INT FOREIGN KEY REFERENCES Cities(CityID)
+--)
+
+--CREATE TABLE Orders
+--(
+--    OrderID INT PRIMARY KEY IDENTITY,
+--	CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID)
+--)
+
+--CREATE TABLE ItemTypes
+--(
+--    ItemTypeID INT PRIMARY KEY IDENTITY,
+--	Name VARCHAR(50) NOT NULL
+--)
+
+--CREATE TABLE Items
+--(
+--    ItemID INT PRIMARY KEY IDENTITY,
+--	Name VARCHAR(50) NOT NULL,
+--	ItemTypeID INT FOREIGN KEY REFERENCES ItemTypes(ItemTypeID)
+--)
+
+--CREATE TABLE OrderItems
+--(
+--    OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+--	ItemID INT FOREIGN KEY REFERENCES Items(ItemID),
+--	CONSTRAINT PK_OrderItem PRIMARY KEY (OrderID, ItemID)
+--)
 
 --06. University Database:
+--CREATE DATABASE Majors
+--Use Majors
+--CREATE TABLE Majors 
+--(
+--    MajorID INT PRIMARY KEY IDENTITY,
+--	Name VARCHAR(50) NOT NULL
+--)
+
+--CREATE TABLE Students
+--(
+--    StudentID INT PRIMARY KEY IDENTITY,
+--	StudentNumber INT NOT NULL,
+--	StudentName VARCHAR(50) NOT NULL,
+--	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+--)
+
+--CREATE TABLE Subjects
+--(
+--    SubjectID INT PRIMARY KEY IDENTITY,
+--	SubjectName VARCHAR(50) NOT NULL
+--)
+
+--CREATE TABLE Agenda
+--(
+--    StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+--	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+--	CONSTRAINT PK_StudentSubject PRIMARY KEY(StudentID, SubjectID)
+--)
+
+--CREATE TABLE Payments
+--(
+--    PaymentID INT PRIMARY KEY IDENTITY,
+--	PaymentDate DATE,
+--	PaymentAmount DECIMAL(15,2),
+--	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+--)
 
 --09. *Peaks in Rila:
 --SELECT m.MountainRange, p.PeakName, p.Elevation 
