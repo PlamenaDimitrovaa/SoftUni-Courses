330df8c4b38945fee09eef60a54d4c639f9c0746
Plamena <plamena3456@abv.bg>
2022-09-07 21:15:03 +0300

    Added Functions And Stored Procedures Exercises.

--- C#Development/C#_DB/MS-SQL/07.Functions-And-Stored-Procedures/FunctionsAndStoredProcedures.sql
+++ C#Development/C#_DB/MS-SQL/07.Functions-And-Stored-Procedures/FunctionsAndStoredProcedures.sql
@@ -92,3 +92,101 @@
 --END
 
 --08. Delete Employees and Departments:
+--CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId INT)
+--AS
+
+--DECLARE @empIDsToBeDeleted TABLE
+--(
+--Id int
+--)
+
+--INSERT INTO @empIDsToBeDeleted
+--SELECT e.EmployeeID
+--FROM Employees AS e
+--WHERE e.DepartmentID = @departmentId
+
+--ALTER TABLE Departments
+--ALTER COLUMN ManagerID int NULL
+
+--DELETE FROM EmployeesProjects
+--WHERE EmployeeID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+--UPDATE Employees
+--SET ManagerID = NULL
+--WHERE ManagerID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+--UPDATE Departments
+--SET ManagerID = NULL
+--WHERE ManagerID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+--DELETE FROM Employees
+--WHERE EmployeeID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+--DELETE FROM Departments
+--WHERE DepartmentID = @departmentId 
+
+--SELECT COUNT(*) AS [Employees Count] FROM Employees AS e
+--JOIN Departments AS d
+--ON d.DepartmentID = e.DepartmentID
+--WHERE e.DepartmentID = @departmentId
+
+--09. Find Full Name:
+--CREATE PROC usp_GetHoldersFullName 
+--AS
+--BEGIN
+--	SELECT FirstName + ' ' + LastName AS [Full Name]
+--	FROM AccountHolders
+--END
+
+--10. People with Balance Higher Than:
+--CREATE PROC usp_GetHoldersWithBalanceHigherThan (@money DECIMAL(15, 2))
+--AS
+--	SELECT FirstName, LastName
+--	FROM AccountHolders ah
+--	JOIN Accounts a ON ah.Id = a.AccountHolderId
+--	GROUP BY FirstName, LastName
+--	HAVING SUM(Balance) > @money
+--	ORDER BY FirstName, LastName
+
+--11. Future Value Function:
+--CREATE FUNCTION ufn_CalculateFutureValue (@InitialSum DECIMAL(18,2), @YearlyInterestRate FLOAT, @NumberOfYears INT)
+--RETURNS DECIMAL(18,4)
+--AS
+--BEGIN
+--    DECLARE @Result DECIMAL(18,4)
+--	SET @Result = @InitialSum * POWER((1 + @YearlyInterestRate), @NumberOfYears)
+--	RETURN @Result
+--END 
+
+--EXEC ufn_CalculateFutureValue 1000, 0.1, 5
+
+--12. Calculating Interest:
+--CREATE PROC	usp_CalculateFutureValueForAccount(@AccountId INT, @InterestRate DECIMAL(18,2))
+--AS
+--BEGIN
+--	SELECT a.Id AS [Account Id],
+--		FirstName AS [First Name], 
+--		LastName AS [Last Name],
+--		Balance AS [Current Balance],
+--		dbo.ufn_CalculateFutureValue(Balance, @InterestRate, 5) AS [Balance in 5 years]
+--	FROM Accounts a
+--	JOIN AccountHolders ah ON ah.Id = a.AccountHolderId
+--	WHERE a.Id = @AccountId
+--END
+
+--13. *Cash in User Games Odd Rows:
+--CREATE FUNCTION ufn_CashInUsersGames(@gameName VARCHAR(MAX))
+--RETURNS @returnedTable TABLE (SumCash MONEY)
+--AS
+--BEGIN
+--	DECLARE @result MONEY
+
+--	SET @result = (SELECT SUM(ug.Cash) AS Cash
+--	FROM (SELECT Cash, GameId, ROW_NUMBER() OVER (ORDER BY Cash DESC) AS RowNumber
+--		FROM UsersGames
+--		WHERE GameId = (SELECT Id FROM Games WHERE Name = @gameName)) AS ug
+--	WHERE ug.RowNumber % 2 != 0)
+
+--	INSERT INTO @returnedTable SELECT @result
+--	RETURN
+--END
