531c499852fda530347c28ac47e926e17cd6319b
Plamena <plamena3456@abv.bg>
2022-11-04 19:44:48 +0200

    Added XML Processing Exercises.

--- C#Development/C#_DB/Entity-Framework-Core/09.XML-Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
+++ C#Development/C#_DB/Entity-Framework-Core/09.XML-Processing/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
@@ -1,10 +1,40 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DataTransferObjects.Input;
+using CarDealer.Models;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            var context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
 
+            var suppliersXml = File.ReadAllText("./Datasets/suppliers.xml");
+
+            var result = ImportSuppliers(context, suppliersXml);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SupplierInputModel[]), new XmlRootAttribute("Suppliers"));
+            var textReader = new StringReader(inputXml);
+            var suppliersDto = xmlSerializer.Deserialize(textReader) as SupplierInputModel[];
+
+            var suppliers = suppliersDto.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            }).ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count()}";
         }
     }
 }
