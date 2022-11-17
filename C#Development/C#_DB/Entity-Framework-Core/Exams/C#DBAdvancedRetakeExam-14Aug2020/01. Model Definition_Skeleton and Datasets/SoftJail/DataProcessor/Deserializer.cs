fe1919a449398b0832ad9b1d8c2768ed7907d59c
Plamena <plamena3456@abv.bg>
2022-11-17 23:24:16 +0200

    Added exam preparation.

--- C#Development/C#_DB/Entity-Framework-Core/Exams/C#DBAdvancedRetakeExam-14Aug2020/01. Model Definition_Skeleton and Datasets/SoftJail/DataProcessor/Deserializer.cs
+++ C#Development/C#_DB/Entity-Framework-Core/Exams/C#DBAdvancedRetakeExam-14Aug2020/01. Model Definition_Skeleton and Datasets/SoftJail/DataProcessor/Deserializer.cs
@@ -2,15 +2,50 @@
 {
 
     using Data;
+    using Newtonsoft.Json;
+    using SoftJail.Data.Models;
+    using SoftJail.DataProcessor.ImportDto;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
 
     public class Deserializer
     {
         public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            var departments = new List<Department>();
+
+            var departmentsCells = JsonConvert.DeserializeObject<IEnumerable<DepartmentCellInputModel>>(jsonString);
+
+            foreach (var departmentCell in departmentsCells)
+            {
+                if (!IsValid(departmentCell) || !departmentCell.Cells.All(IsValid) || !departmentCell.Cells.Any())
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                var department = new Department
+                {
+                    Name = departmentCell.Name,
+                    Cells = departmentCell.Cells.Select(x => new Cell
+                    {
+                        CellNumber = x.CellNumber,
+                        HasWindow = x.HasWindow
+                    })
+                    .ToList()
+                };
+
+                departments.Add(department);
+                sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+            }
+
+            context.Departments.AddRange(departments);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
