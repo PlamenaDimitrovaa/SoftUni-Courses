60582ff9f164e2d6bc7fd460c3aa874117c7c0f4
Plamena <plamena3456@abv.bg>
2022-12-03 23:06:42 +0200

    Added exam preparation.

---
+++ C#Development/C#_DB/Entity-Framework-Core/Exams/C#DBAdvancedRetakeExam-16Dec2021/01. Model Defition_Skeleton/Skeleton/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 4)]
+        public string ManufacturerName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Founded { get; set; }
+        public ICollection<Gun> Guns { get; set; }
+    }
+}
