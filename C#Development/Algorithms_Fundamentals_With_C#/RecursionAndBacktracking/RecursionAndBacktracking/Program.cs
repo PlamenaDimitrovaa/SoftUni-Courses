49c63987ef04a7dda3936519dad54c7fabd30991
Plamena <plamena3456@abv.bg>
2022-12-06 22:57:38 +0200

    Added Algorithms Fundamentals Recursion and Backtracking Exercise.

---
+++ C#Development/Algorithms_Fundamentals_With_C#/RecursionAndBacktracking/RecursionAndBacktracking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Recursive Array Sum
+            int[] arr = { 1,2,3,4 };
+            Console.WriteLine(GetSum(arr, 1));
+        }
+
+        //1.Recursive Array Sum
+            private static int GetSum(int[] arr, int index)
+            {
+                if (index >= arr.Length)
+                {
+                    return 0;
+                }
+
+                return arr[index] + GetSum(arr, index + 1);
+            }
+    }
+}
