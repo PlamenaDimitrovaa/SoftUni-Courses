59631c55f0559f46085e1ca5680f0a330be4dbfb
Plamena <plamena3456@abv.bg>
2022-12-08 17:20:49 +0200

    Added Recursion and Backtracking - Lab Exercise.

---
+++ C#Development/Algorithms_Fundamentals_With_C#/RecursionAndBacktracking/04.FactorielRecursively/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalcFactorielRecursively(n));
+        }
+
+        private static int CalcFactorielRecursively(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalcFactorielRecursively(n - 1);
+        }
+    }
+}
--- C#Development/Algorithms_Fundamentals_With_C#/RecursionAndBacktracking/RecursionAndBacktracking/Program.cs
+++ C#Development/Algorithms_Fundamentals_With_C#/RecursionAndBacktracking/RecursionAndBacktracking/Program.cs
@@ -7,12 +7,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main()
         {
-            //1.Recursive Array Sum
             var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
             Console.WriteLine(GetSum(arr, 0));
         }
 
-        //1.Recursive Array Sum
             private static int GetSum(int[] arr, int index)
             {
                 if (index >= arr.Length)
