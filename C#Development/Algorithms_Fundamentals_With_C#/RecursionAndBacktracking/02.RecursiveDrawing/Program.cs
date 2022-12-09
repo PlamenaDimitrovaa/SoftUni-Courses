ee10349617f66aad13ea06d63192933e36feffa7
Plamena <plamena3456@abv.bg>
2022-12-09 23:43:08 +0200

    Added Recursion Exercise.

---
+++ C#Development/Algorithms_Fundamentals_With_C#/RecursionAndBacktracking/02.RecursiveDrawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.RecursiveDrawing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int row)
+        {
+            if (row == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', row));
+            DrawFigure(row - 1);
+            Console.WriteLine(new string('#', row));
+        }
+    }
+}
