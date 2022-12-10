1cd59e8538ae504396752efb2e26bbd55366f2c2
Plamena <plamena3456@abv.bg>
2022-12-10 16:04:20 +0200

    Added Recursion And Backtracking Exercises.

---
+++ C#Development/Algorithms_Fundamentals_With_C#/RecursionAndBacktracking/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.RecursiveFibonacci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalcFibonacci(n));
+        }
+
+        static long CalcFibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+
+            return CalcFibonacci(number - 1) + CalcFibonacci(number - 2);
+        }
+    }
+}
