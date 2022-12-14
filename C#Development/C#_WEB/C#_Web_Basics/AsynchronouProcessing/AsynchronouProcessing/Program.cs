a2bcf05a2ae0f365239a703bbf56a2aec610ba3d
Plamena <plamena3456@abv.bg>
2022-12-15 00:58:00 +0200

    Added Asynchronous Processing Exercise.

---
+++ C#Development/C#_WEB/C#_Web_Basics/AsynchronouProcessing/AsynchronouProcessing/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AsynchronouProcessing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintNumbersInRange(0, 100);
+
+            Task task = Task.Run(() => PrintNumbersInRange(100, 200));
+            Task newTask = Task.Run(() => PrintNumbersInRange(500, 600));
+
+            Console.WriteLine("Done");
+
+            task.Wait();
+            newTask.Wait();
+        }
+
+        static void PrintNumbersInRange(int min, int max)
+        {
+            for (int i = min; i < max; i++)
+            {
+                Console.WriteLine($"{i},");
+            }
+        }
+    }
+}
---
+++ C#Development/C#_WEB/C#_Web_Basics/AsynchronouProcessing/PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrintEvenNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Thread thread = new Thread(() => PrintEvenNumbers(a, b));
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("Thread Finished Work");
+        }
+
+        static void PrintEvenNumbers(int a, int b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
