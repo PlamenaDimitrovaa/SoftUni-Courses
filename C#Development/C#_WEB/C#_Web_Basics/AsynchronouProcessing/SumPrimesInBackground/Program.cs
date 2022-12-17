a5c3c8cbeb381e48c6bb051caa26d48fe4206f1f
Plamena <plamena3456@abv.bg>
2022-12-17 17:25:44 +0200

    Added Asynchronous Processing Exercise.

---
+++ C#Development/C#_WEB/C#_Web_Basics/AsynchronouProcessing/SumPrimesInBackground/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SumPrimesInBackground
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long sum = 0;
+
+            Task.Run(() =>
+            {
+                for (long i = 0; i < 1000000000; i++)
+                {
+                    sum += 1;
+                    Thread.Sleep(10);
+                }
+            });
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "show")
+                {
+                    Console.WriteLine(sum);                    
+                }
+                else if(command == "exit") 
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
