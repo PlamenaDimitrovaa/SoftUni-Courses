f76035157ed3ea13d6c9f1e1166bf5ca5400f6fd
Plamena <plamena3456@abv.bg>
2023-02-23 16:32:56 +0200

    Added Mini ORM .NET6.

---
+++ C#Development/C#_DB/Entity-Framework-Core/02.ORM-Fundamentals/02.ORM-Fundamentals-Exercise-MiniORM-Skeleton-6.0/MiniORM.App/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniORM.App
+{
+    public static class Config
+    {
+        public const string ConnectionString =
+            "Server=DESKTOP-F0G7R2C;Database=MiniORM;Integrated Security=true;Encrypt=false;";
+
+    }
+}
+
