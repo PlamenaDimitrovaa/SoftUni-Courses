87252d7938ef3501f7cbae2856067f40860329c3
Plamena <plamena3456@abv.bg>
2023-03-03 23:00:05 +0200

    Added Entity Relations Exercises on EF 6

---
+++ C#Development/C#_DB/Entity-Framework-Core/04.Entity-Relations/EntityRelations-Exercises/P01_StudentSystem.Data.Models/Course.cs
@@ -0,0 +1,39 @@
+﻿using P01_StudentSystem.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.Students = new HashSet<Student>();
+            this.Resources = new HashSet<Resource>();
+            this.Homeworks = new HashSet<Homework>();
+            this.StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(80)")]
+        [MaxLength(ValidationConstants.CourseNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual ICollection<Resource> Resources { get; set; }
+        public virtual ICollection<Homework> Homeworks { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
---
+++ C#Development/C#_DB/Entity-Framework-Core/04.Entity-Relations/EntityRelations-Exercises/P01_StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using P01_StudentSystem.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            this.Courses = new HashSet<Course>();
+            this.Homeworks = new HashSet<Homework>();
+            this.StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")] //???
+        [MaxLength(ValidationConstants.StudentNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(10)]
+        public string? PhoneNumber { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+
+        public virtual ICollection<Homework> Homeworks { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
