c55917f33c041cb520a080013c3f6787c84377fa
Plamena <plamena3456@abv.bg>
2023-03-16 23:27:13 +0200

    Added JSON Processing Exercises.

---
+++ C#Development/C#_DB/Entity-Framework-Core/08.JSON-Processing/08.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/StartUp.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            string inputJson = File.ReadAllText(@"../../../Datasets/categories-products.json");
+            string result = ImportCategoryProducts(context, inputJson);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            var userDtos = JsonConvert.DeserializeObject<ImportUserDTO[]>(inputJson);
+
+            ICollection<User> validUsers = new HashSet<User>();
+
+            foreach (var userDto in userDtos)
+            {
+                User user = mapper.Map<User>(userDto);
+                validUsers.Add(user);
+            }
+
+            context.Users.AddRange(validUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validUsers.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+            var productDtos = JsonConvert.DeserializeObject<ImportProductDTO[]>(inputJson);
+
+            Product[] products = mapper.Map<Product[]>(productDtos);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            ImportCategoryDTO[] categoryDtos = JsonConvert.DeserializeObject<ImportCategoryDTO[]>(inputJson);
+
+            ICollection<Category> validCategories = new HashSet<Category>();
+
+            foreach (var categoryDto in categoryDtos)
+            {
+                if (String.IsNullOrEmpty(categoryDto.Name))
+                {
+                    continue;
+                }
+
+                Category category = mapper.Map<Category>(categoryDto);
+                validCategories.Add(category);
+            }
+
+            context.Categories.AddRange(validCategories);
+            context.SaveChanges();
+
+            return $"Successfully imported {validCategories.Count}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            var cpDtos = JsonConvert.DeserializeObject<ImportCategoryProductDTO[]>(inputJson);
+
+            ICollection<CategoryProduct> validEntries = new HashSet<CategoryProduct>();
+
+            foreach (ImportCategoryProductDTO cpDto in cpDtos)
+            {
+                if (!context.Categories.Any(c => c.Id == cpDto.CategoryId) ||
+                        !context.Products.Any(p => p.Id == cpDto.ProductId))
+                {
+                    continue;
+                }
+
+                CategoryProduct categoryProduct = mapper.Map<CategoryProduct>(cpDto);
+                validEntries.Add(categoryProduct);
+            }
+
+            context.CategoriesProducts.AddRange(validEntries);
+            context.SaveChanges();
+
+            return $"Successfully imported {validEntries.Count}";
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+        }
+    }
+}
